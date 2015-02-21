701b4412df2b4fa6e8f3b112a43b8c7321ab20db
Kevin Collette <kevcollette@gmail.com>
2015-02-21 10:18:22 -0600

    Add proper defaults to model attributes.

--- image_manipulation/models.py
+++ image_manipulation/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Image class instantiates as a row in the table
 
 class Image(models.Model):
-  image_path = models.CharField()
+  image_path = models.CharField(max_length=1000)
   user = models.ForeignKey(User)
-  private = models.BooleanField()
+  private = models.BooleanField(default=True)
 
   def __str__(self):
     return self.image_path
