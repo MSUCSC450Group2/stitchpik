29d18f23387bf35b96cb88cfbd283d43f9b6b47d
goodwinnadiia <goodwin09@live.missouristate.edu>
2015-02-22 21:44:49 -0600

    Copy folders and files from login to WebPages: __pycache__, migrations, forms.py, models.py, tests.py, views.py. Create view for index page; views.py and tests.py paths should be adjusted.

--- WebPages/tests.py
+++ WebPages/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from login.forms import *
 
-# Create your tests here.
+
+# Create your tests here
+class RegistrationTestCase(TestCase):
+
+    def testRedirect(self):
+        # calls redirect url
+        response = Client().post('/accounts/login')
+        self.assertEqual(response.status_code, 301)
+        
+	
+    def testActivePage(self):
+        response = self.client.get('/accounts/login', follow=True)
+        self.assertEqual(response.status_code, 200, "login page not working")
+
