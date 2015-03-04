8c9644ce91c4ce265de329948aa2844512fa5e47
goodwinnadiia <goodwin09@live.missouristate.edu>
2015-03-03 23:15:13 -0600

    Add url for index page. Add names for login and application.

--- stitchpick/urls.py
+++ stitchpick/urls.py
@@ -5,17 +5,18 @@ from image_manipulation.views import *
 #    ^ this might be need to be changed to point correctly
 
 urlpatterns = patterns('',
+    url(r'^$', 'login.views.index', name='index'),	       
     # Examples:
     # url(r'^$', 'stitchpick.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^$', 'django.contrib.auth.views.login'),
 	#          \/ will be changed to main index view
-    url(r'^$', loginTry),
+    url(r'^login/$', loginTry, name='login'),
     url(r'^accounts/login/$', loginTry),
     url(r'^admin/$', include(admin.site.urls)), 
     url(r'^logout/$', logoutPage),
     url(r'^registration/$', registration, name ='signup'),
     url(r'^registration/success/$', registrationSuccess),
-    url(r'^application/$', fetchApplication),
-    url(r'^home/$', home),
+    url(r'^application/$', fetchApplication, name='application'),
+    #url(r'^home/$', home),
 )
