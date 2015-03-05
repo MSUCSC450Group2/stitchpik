f479988d4711647ecfd6926f6949bf8b86e87ebd
Zach <zsgilmer@gmail.com>
2015-03-04 20:44:49 -0600

    Added pixelization to application if page loads before image finishes processing it will not display, but it still pixelates.

--- image_manipulation/views.py
+++ image_manipulation/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from .forms import *
+from .manipulate_lib.imageclass import *
+
 
 # Create views here
 
@@ -16,15 +18,24 @@ def fetchApplication(request):
         form = ManipulateImageForm(request.POST)
         if form.is_valid():
             # Use data to serve up image
+            numColors = form.cleaned_data['numberOfColors']
+            pixSize = 8
             
-            # put image in variables field
+            pic = Picture('image_manipulation/static/image_manipulation/img/bubblegum.jpg')
+            resultImage = 'image_manipulation/static/image_manipulation/img/bubblegum2.jpg'
+            #currently runs properly every time, if image is not done processing when page loads the image is not displayed.
+            #can possibly fix with adding a delayed load to the image through jquery or similar.
+            pic.pixelate(numColors, pixSize, resultImage)
+	        # put image in variables field
             # variables = RequestContext(request, { 'image':'' })
-            variables = RequestContext(request, { 'form':form, 'image':'image_manipulation/img/bubblegum.jpg'})
+
+
+            variables = RequestContext(request, { 'form':form, 'image':'image_manipulation/img/bubblegum2.jpg'})
             return render_to_response('image_manipulation/applicationPage.html', variables)
     else:
         form = ManipulateImageForm()
     
-    variables = RequestContext(request, { 'image':'image_manipulation/img/bubblegum.jpg', 'form':form })
+    variables = RequestContext(request, { 'form':form, 'image':'image_manipulation/img/bubblegum.jpg'})
         
     return render_to_response('image_manipulation/applicationPage.html', variables)     
 
