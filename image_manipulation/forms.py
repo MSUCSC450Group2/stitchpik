dee71825c4ad30c2e4eb41ecae27e65f69d076e3
goodwinnadiia <goodwin09@live.missouristate.edu>
2015-04-17 12:47:54 -0500

    Add restrictions on negative values of width and height.

--- image_manipulation/forms.py
+++ image_manipulation/forms.py
@@ -13,8 +13,8 @@ class ManipulateImageForm(forms.Form):
   numberOfColors = forms.IntegerField(max_value=32, min_value=2, 
                                         label='Number of Colors', initial='8')
   guageSize = forms.DecimalField(label="Guage Size", initial='10', decimal_places=2)
-  canvasLength = forms.DecimalField(label="Canvas Length (in)", initial='12', decimal_places=2)
-  canvasWidth = forms.DecimalField(label="Canvas Width (in)", initial='12', decimal_places=2)
+  canvasLength = forms.DecimalField(label="Canvas Length (in)", initial='12', decimal_places=2, min_value=0)
+  canvasWidth = forms.DecimalField(label="Canvas Width (in)", initial='12', decimal_places=2, min_value=0)
   knitType = forms.ChoiceField(widget=forms.RadioSelect, choices=KNIT_TYPES, 
                                  label="Knit Type", initial="0")
     
